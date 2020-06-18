81add6536cef29b961fa7ce77f7efa6ce2a03e52
Manu Blanco Valentín <manuelblancovalentin@users.noreply.github.com>
2020-06-18 17:46:00 -0300

    distribution files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DynamicTable',
+    version='0.0.1',
+    packages=['DynamicTable'],
+    url='http://github.com/manuelblancovalentin/DynamicTable',
+    license='GPL',
+    author='Manu Blanco Valentin',
+    author_email='manuel.blanco.valentin@gmail.com',
+    description=''
+)
